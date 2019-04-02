63e3665d767e6f6375d3386af69d964c9d9a6fd7
ddpalacios <45171403+ddpalacios@users.noreply.github.com>
2019-04-01 19:05:01 -0500

    Update dog-cat-classification.py

--- dog-cat-classification.py
+++ dog-cat-classification.py
@@ -4,27 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-input_size = 1000
-hidden_size = 500
-num_classes = 10
-num_epochs = 5
-batch_size = 100
-learning_rate = 0.001
 
-
-
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size) 
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)  
-    
-    def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        return out
 
 ModifyPics=transforms.Compose([transforms.Resize(100),
                                     transforms.CenterCrop(100),
